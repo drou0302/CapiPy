f89568207cde612b3c29569bf0e2e0c982397172
David Roura Padrosa <67318891+drou0302@users.noreply.github.com>
2020-10-19 16:56:31 +0200

    Update Uninstall.bat

--- Installation/Uninstall.bat
+++ Installation/Uninstall.bat
@@ -2,5 +2,9 @@
 echo "Starting with environment deletion...""
 call conda env remove --name vCapiPy >> Uninstall.log 2>&1
 del ../*.bat 
+
+cd Environment
+del *.log
+
 echo "Uninstall complete. Check Uninstall.log for more information."
 pause
