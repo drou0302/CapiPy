07bf99611b566f09e790b6da8cbfa869598b431d
David Roura Padrosa <davidrourap@gmail.com>
2020-11-23 11:47:25 +0100

    Updated
    Update 0.0.2

--- Install_CapiPy.bat
+++ Install_CapiPy.bat
@@ -1,18 +1,17 @@
 @echo off
-echo Creating vCapiPy environment...
-call conda env create -f Environment/vCapiPy.yml >> ./Environment/env_creation.log 2>&1
-echo Environment created
+ECHO Step 1/3: Creating vCapiPy environment...
+CALL conda env create -f Environment/vCapiPy.yml > ./ENV-CREATION.log
+ECHO Environment creation finished!
+ECHO ____________________________________________________________________________
+TIMEOUT /t 3 /nobreak > NUL
 
-echo Copying files...
-copy ".\Environment\CapiPy.bat" "..\" >> ./Environment/copyfiles.log 2>&1
-set /p LICENSE="Enter your MODELLER license (https://salilab.org/modeller/registration.html):"
-call python ./Environment/file_modification.py %1 %LICENSE% >> ./Environment/copyfiles2.log 2>&1
-echo Finished copying files.
+ECHO Step 2/3: File modification to ensure CapiPy's correct operation...
+MOVE %~dp0\Environment\CapiPy.bat %~dp0\ > NUL
+SET /p LICENSE="Enter your MODELLER license (https://salilab.org/modeller/registration.html):"
+CALL python ./Environment/file_modification.py %1 %LICENSE% > CON
+ECHO File modification finished...
+TIMEOUT /t 3 /nobreak > NUL
+ECHO ____________________________________________________________________________
 
-cd Environment
-type NUL > installation.log
-copy *.log  installation.log
-copy installation.log  ..\
-
-echo Installation complete. Check installation.log for errors.
+ECHO Installation complete. Check both log files for errors.
 pause
--- Install_CapiPy.sh
+++ Install_CapiPy.sh
@@ -2,22 +2,17 @@
 echo "Indicate the full location of conda.sh"
 read $CONDA 
 source $CONDA
-
 echo Creating vCapPy environment...
-conda env create -f ./Environment/vCapiPy.yml > ./env_creation.log 2>&1
+conda env create -f ./Environment/vCapiPy.yml > ./ENV-CREATION.log 
 echo Environment created.
+echo ____________________________________________________________________________
 
-echo Copying files...
 cp Environment/*.sh ../
 echo "Enter your MODELLER license (https://salilab.org/modeller/registration.html):"
 read $LICENSE 
-python ./Environment/file_modification.py $1 $LICENSE >> copyfiles1.log 2>&1
-
-cd Environment
-touch installation.log
-cd ..
-cp ./Environment/installation.log ./
-echo Finished copying files.
+python ./Environment/file_modification.py $1 $LICENSE > CON
+echo File modification finished...
+echo ____________________________________________________________________________
 
 read -rsp $'Press enter to close the finish.\n'
 
